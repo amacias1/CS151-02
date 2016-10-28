36870cb014d3429b3158a5297e2c4481d580eea0
Abel M <abel29170@gmail.com>
2016-10-28 11:32:23 -0400

    Revert "start of stack interface"
    This reverts commit 4befbb6dd15d14c89124b50e737924150f6176cb.

--- src/sparta/Stack.java
+++
@@ -1,5 +0,0 @@
-package sparta;
-
-public class Stack {
-
-}
--- src/sparta/stackInterface.java
+++
@@ -1,7 +0,0 @@
-package sparta;
-
-public abstract class stackInterface {
-	public abstract void add(Object obj);
-	public abstract void remove();
-	
-}
